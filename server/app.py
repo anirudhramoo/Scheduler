6fca9bc8b9a5d150103e742dab1bd9b035c1ee94
beloiual <beloiual@umich.edu>
2024-02-17 04:39:43 -0800

    Added entire microphone integration on backend and frontend

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
-from server.endpoints.test import handle_test
+from endpoints.test import handle_test
+from endpoints.process_audio import handle_audio
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.add_url_rule('/test', 'handle_test', handle_test, methods=['POST'])
+app.add_url_rule('/process-audio', 'handle_audio', handle_audio, methods=['POST'])
 
 if __name__ == '__main__':
-    app.run(port=3001)
+    app.run(port=5000)
